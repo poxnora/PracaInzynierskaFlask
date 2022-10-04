ce362c2867742194403d6d733e5cb8c478cd05be
Jakub Wójcik <kwojcik99@gmail.com>
2022-10-04 09:24:05 +0200

    config

--- backend/configuration/Config.py
+++ backend/configuration/Config.py
@@ -3,6 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+# PYTHONUNBUFFERED=1;POSTGRES_HOST=localhost;POSTGRES_USER=user;POSTGRES_PASSWORD=user;POSTGRES_PORT=5433;POSTGRES_DB=postgres
 class Config:
     user = os.environ['POSTGRES_USER']
     password = os.environ['POSTGRES_PASSWORD']
@@ -14,12 +15,14 @@ class Config:
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TEMPLATES_FOLDER = (os.path.abspath(os.path.join(__file__, "../../..", 'frontend', 'templates')))
-    STATIC_FOLDER=(os.path.abspath(os.path.join(__file__, "../../..", 'frontend', 'static')))
+    STATIC_FOLDER = (os.path.abspath(os.path.join(__file__, "../../..", 'frontend', 'static')))
+
 
 class ProdConfig(Config):
     FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
+
 
 class DevConfig(Config):
     FLASK_ENV = 'development'
